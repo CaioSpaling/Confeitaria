74cb95ba688569fe8afff1da0d3d0fe943d16658
CaioSpaling <caiospaling@gmail.com>
2025-11-26 20:00:00 -0300

    Update Style

--- cardapio.js
+++ cardapio.js
@@ -1,6 +1,9 @@
-function criarItemCardapio() {
+function criarItemCardapio(titulo, descricao) {
     const divItemCardapio = document.createElement('div')
     //colocar classe
     
     const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
+
+    const pDescricao = document
 }
